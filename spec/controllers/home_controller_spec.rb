825517ae9253145de4ad917e806a543f3d5f21bb
dinusha-b14 <dinusha_b@hotmail.com>
2017-03-25 20:19:15 +1100

    updated specs for home controller and added simple cov test coverage (#3)

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,13 +3,24 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
   render_views
   let(:user) { create(:user) }
-  before { sign_in(user) }
 
   describe "GET #index" do
     context 'when signed in' do
-      it "returns http success" do
+      before do
+        sign_in(user)
         get :index
+      end
+
+      it "returns http success" do
         expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when not signed in' do
+      before { get :index }
+
+      it 'redirects to sign_in page' do
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
